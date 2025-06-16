5292a072e03378c716d0daacc99e72767b415a98
Miss_Christie <christianakobea@gmail.com>
2025-06-16 12:52:53 +0000

    m

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,7 @@ import google from "../assets/images/google-icon.png";
 
 export default function Register() {
     return (
-        <div className="bg-gray-200 min-h-screen">
+        <div className="bg-gray-200 h-screen">
             {/* <div className="container mx-auto"> */}
                 <div className="flex flex-row w-full bg-gray-100 rounded-md mx-auto overflow-hidden">
                     <div className="w-1/2 bg-cover" style={{ backgroundImage: `url(${(register)})` }}>
