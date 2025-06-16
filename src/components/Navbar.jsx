e3cd907c0d6c57bfa383049fc83f5e001867aa68
James Junior Baah <jjbaah068@gmail.com>
2025-06-16 10:19:24 +0000

    linking pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router";
+
+
 export default function Navbar() {
     return (
         <nav className="flex justify-between px-8 py-4">
@@ -6,8 +9,14 @@ export default function Navbar() {
                 <span className="text-primary"> Hive</span>
             </h1>
             <div className="flex justify-center gap-10 mr-10">
-                <button className="text-black"><a href="#">Login</a></button>
-                <button className="bg-primary rounded-md py-3 px-10 text-white"><a href="#">Signup</a></button>
+                <button className="text-black"><Link to="/login">
+                 Login
+                </Link>
+                   
+                    </button>
+                <button className="bg-primary rounded-md py-3 px-10 text-white"><Link to="/register">
+                Signup
+                 </Link></button>
             </div>
         </nav>
     );
