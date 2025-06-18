5c16cba4991a780eb271ae0164d177346987ead7
Miss_Christie <christianakobea@gmail.com>
2025-06-18 15:15:56 +0000

    Worked on login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import login from "../assets/images/login.png";
 import google from "../assets/images/google-icon.png";
 import SubmitButton from "../components/SubmitButton";
 import { apiClient } from "../api/client";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 export default function Login() {
     const navigate = useNavigate();
@@ -25,16 +25,16 @@ export default function Login() {
     }
 
     return (
-        <div className="bg-gray-200 h-screen">
+        <div className="bg-gray-200 h-screen overflow-hidden">
             {/* <div className="container mx-auto"> */}
-            <div className="flex flex-row w-full bg-gray-100 rounded-md mx-auto overflow-hidden">
-                <div className="w-1/2 py-16 px-12">
+            <div className="flex flex-col md:flex-row w-full bg-gray-100 rounded-md overflow-hidden h-screen">
+                <div className="md:w-1/2 py-16 px-12 w-full">
                     <p className="text-center text-2xl font-bold">Event <span className="text-[#005DB5]">Hive</span></p>
-                    <h2 className="text-center text-4xl font-bold mt-12 mb-10 ml-10">Sign In to Event Hive</h2>
+                    <h2 className="text-center md:text-4xl text-3xl font-bold mt-10 mb-12 ml-10">Sign In to Event Hive</h2>
 
                     <form action={loginUser} className="">
 
-                        <label className="block uppercase text-md mb-2">Your Email</label>
+                        <label className="block uppercase text-sm mb-2">Your Email</label>
 
                         <input type="email"
                             name="email"
@@ -42,8 +42,8 @@ export default function Login() {
                             required
                             className="border border-gray-100 rounded-md bg-white w-full text-gray-700 text-md px-4 py-2 mb-6" />
                         <div className="flex flex-row justify-between">
-                            <label className="block uppercase text-md mb-2">Password</label>
-                            <label className="block  text-md mb-2 text-gray-500 mr-1">Forgot your password?</label>
+                            <label className="block uppercase text-sm mb-2">Password</label>
+                            <label className="block  text-sm mb-2 text-gray-500 mr-1">Forgot your password?</label>
                         </div>
 
                         <input type="password"
@@ -54,28 +54,28 @@ export default function Login() {
                             placeholder="Enter your password"
                             className="border border-gray-100 rounded-md bg-white w-full text-gray-700 text-md px-4 py-2 mb-6" />
 
-                        <SubmitButton title={'Sign In'} className="border border-gray-100 rounded-md text-white text-md px-2 py-3 w-2/4 mt-6 ml-33 bg-[#7848F4]" />
-                        <p className="mt-6 text-center">Or</p>
-                        <div className="flex flex-row border border-gray-100 rounded-md bg-white text-gray-700 text-sm px-6 py-3 w-2/3 mt-6 ml-20 mb-10">
-                            <img src={google} className="h-6 w-6 text-center ml-15" />
-                            <p className="text-center text-lg ml-4 mr-2">Sign up with Google</p>
-
-
-                        </div>
+                        <SubmitButton title={'Sign In'} className="border border-gray-100 rounded-md text-white text-md px-2 py-3 w-2/4 mt-5 md:ml-33 ml-25 bg-[#7848F4]" />
                     </form>
-                </div>
+                    <p className="mt-5 text-center">Or</p>
+                    <div className="flex flex-row justify-center border border-gray-100 rounded-md bg-white text-gray-700 text-sm px-6 py-3 w-3/4 mt-6 ml-12 md:ml-20 mb-13">
+                        <img src={google} className="h-6 w-6 text-center md:ml-15 ml-4" />
+                        <p className=" text-lg ml-4 mr-2">Sign up with Google</p>
 
-                <div className="w-1/2 bg-cover" style={{ backgroundImage: `url(${(login)})` }}>
+                    </div>
+                </div>
+                    <div className="w-1/2">
+                <div className="bg-cover bg-center h-screen bg-no-repeat" style={{ backgroundImage: `url(${(login)})` }}>
                     <div className="flex flex-col text-white">
                         <h1 className="text-center mt-65 mb-6 text-4xl font-bold">Hello Friend</h1>
                         <p className="text-2xl text-center mb-8">To keep connected with us provide us with your information </p>
 
-                        <button type="submit"
+                        <Link to={"/register"} type="submit"
                             className="block border border-[#B4B9C2] rounded text-white text-md py-1 px-4 w-20 h-8 bg-[#B4B9C2] whitespace-nowrap ml-70">
                             Sign Up
-                        </button>
+                        </Link>
                     </div>
 
+                </div>
                 </div>
             </div>
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,7 @@ import register from "../assets/images/register.png";
 import google from "../assets/images/google-icon.png";
 import SubmitButton from "../components/SubmitButton.jsx";
 import { apiClient } from "../api/client.js";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 export default function Register() {
     const navigate = useNavigate();
@@ -23,46 +23,47 @@ export default function Register() {
         }
     }
     return (
-        <div className="bg-gray-200 h-screen">
+        <div className="bg-gray-200 w-full h-screen overflow-hidden">
             {/* <div className="container mx-auto"> */}
-            <div className="flex flex-row w-full bg-gray-100 rounded-md h-full">
-                <div className="w-1/2 bg-cover h-full" style={{ backgroundImage: `url(${(register)})` }}>
+            <div className="flex flex-row w-full bg-gray-100 rounded-md h-screen overflow-hidden">
+                <div className="w-1/2">
+                <div className="bg-cover bg-center h-screen bg-no-repeat " style={{ backgroundImage: `url(${(register)})` }}>
                     <div className="flex flex-col justify-center text-white h-full">
                         <h1 className="text-center mt-6 mb-6 text-4xl font-bold">Welcome back</h1>
                         <p className="text-2xl text-center mb-8">To keep connected with us provide us with your information </p>
-                        <button type="submit" className="block border border-[#B4B9C2] rounded text-white text-md py-1 px-2 w-20 h-8 bg-[#B4B9C2] whitespace-nowrap ml-70 text-center">Sign In</button>
+                        <Link to={"/login"} type="submit" className="block border border-[#B4B9C2] rounded text-white text-md py-1 px-2 w-20 h-8 bg-[#B4B9C2] whitespace-nowrap ml-70 text-center">Sign In</Link>
+                    </div>
                     </div>
 
                 </div>
 
                 <div className="w-1/2 px-12 h-full">
                     <p className="text-center text-2xl font-bold mt-8">Event <span className="text-[#005DB5]">Hive</span></p>
-                    <h2 className="text-center text-4xl font-bold mt-10 mb-2 ml-10">Sign Up to Event Hive</h2>
+                    <h2 className="text-center text-4xl font-bold mt-8 mb-2 ml-10">Sign Up to Event Hive</h2>
                     <form action={resgiterUser}>
                         <label className="block uppercase text-md mb-2">Your Name</label>
-                        <input type="text" name="name" placeholder="Enter your name" required className="border border-gray-100 rounded-md bg-white w-full text-gray-700 text-md px-4 py-2 mb-8" />
+                        <input type="text" name="name" placeholder="Enter your name" required className="border border-gray-100 rounded-md bg-white w-full text-gray-700 text-md px-4 py-2 mb-7" />
 
                         <label className="blockn uppercase text-md">Email</label>
-                        <input type="email" name="email" placeholder="Enter your name" required className="border border-gray-100 rounded-md bg-white w-full text-gray-700 text-md px-4 py-2 mt-2 mb-8" />
+                        <input type="email" name="email" placeholder="Enter your name" required className="border border-gray-100 rounded-md bg-white w-full text-gray-700 text-md px-4 py-2 mt-2 mb-7" />
 
                         <label className="block uppercase text-md mb-2">Password</label>
-                        <input type="password" name="password" required minlength="8" maxlength="16" pattern="^[a-zA-Z0-9_]*${16}" placeholder="Enter your password" className="border border-gray-100 rounded-md bg-white w-full text-gray-700 text-md px-4 py-2 mb-8" />
+                        <input type="password" name="password" required minlength="8" maxlength="16" pattern="^[a-zA-Z0-9_]*${16}" placeholder="Enter your password" className="border border-gray-100 rounded-md bg-white w-full text-gray-700 text-md px-4 py-2 mb-7" />
 
                         {/* <label className="block uppercase text-md mb-2">Confirm Password</label>
                         <input type="password" placeholder="Enter your password" minlength="8" maxlength="16" pattern="^[a-zA-Z0-9_]*${16}" required className="border border-gray-100 rounded-md bg-white w-full text-gray-700 text-md px-4 py-2 mb-8 " /> */}
 
                         <SubmitButton
-                            className="border border-gray-100 rounded-md text-white text-sm px-2 py-3 w-2/4 mt-6 ml-33 bg-[#7848F4]"
+                            className="border border-gray-100 rounded-md text-white text-md px-2 py-3 w-2/4 mt-2 ml-33 bg-[#7848F4]"
                             title={"Sign Up"} />
+                    </form>
 
-
-                        <p className="mt-6 text-center">Or</p>
-                        <div className="flex flex-row border border-gray-100 rounded-md bg-white text-gray-700 text-sm px-6 py-3 w-2/3 mt-6 ml-20 mb-10">
+                         <p className="mt-5 text-center mr-7">Or</p>
+                        <div className="flex flex-row border border-gray-100 rounded-md bg-white text-gray-700 text-sm px-6 py-2.5 w-2/3 mt-5 ml-20">
                             <img src={google} className="h-6 w-6 text-center ml-15" />
                             <p className="text-center text-lg ml-4 mr-2">Sign up with Google</p>
 
                         </div>
-                    </form>
                 </div>
             </div>
 
