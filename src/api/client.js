9a5db699c4628442f1818166251ee24c509b4af3
James Junior Baah <jjbaah068@gmail.com>
2025-06-18 14:57:35 +0000

    trending-colleges integration

--- src/api/client.js
+++ src/api/client.js
@@ -3,3 +3,10 @@ import axios from "axios";
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL
 });
+ 
+export const apiFetcher = async (url) => {
+    const response = await apiClient.get(url);
+    return response.data;   
+}
+
+export const imageBaseURL = import.meta.env.VITE_IMAGE_BASE_URL;
