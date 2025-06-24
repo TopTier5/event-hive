63540818a8ccc1c558b8605c89a0ddb638e7341a
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-06-24 16:05:21 +0000

    changed something in eventcard

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -5,7 +5,7 @@ export default function EventCard({event}) {
     return (
         <div className="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 py-6">
             <div className="rounded-md overflow-hidden relative">
-                <img src={`${imageBaseURL}/${event.image}`} 
+                <img src={event.image}
             alt="Event Card" className="px-4 py-2 rounded-md" />
             <span className="absolute top-3 left-7 mt-3 text-[#7848f4] bg-white rounded px-2 py-1 text-sm font-semibold">FREE</span>
 
